41af7d9807436e5f6973ab0ca13f2bed2d0201ae
Ellen <mail@ellen-schrader.de>
2024-12-26 15:31:57 +0000

    commenting option

--- articles/admin.py
+++ articles/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 # Register your models here.
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("comment", "author")
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title", "body", "author", "date")
+    inlines = [CommentInline]
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/views.py
+++ articles/views.py
@@ -1,19 +1,17 @@
-from django.views.generic import ListView, DetailView
+from django.views import View
+from django.views.generic import ListView, DetailView, FormView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.views.generic.detail import SingleObjectMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
+from .forms import CommentForm
 from .models import Article
 
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
-
-
-class ArticleDetailView(LoginRequiredMixin, DetailView):
-    model = Article
-    template_name = "article_detail.html"
 
 
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -36,7 +34,9 @@ class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return obj.author == self.request.user
 
 
-class ArticleCreateView(LoginRequiredMixin, CreateView): # LoginRequiredMixin should come first in the inheritance list to ensure that the user is authenticated before creating an article.
+class ArticleCreateView(
+    LoginRequiredMixin, CreateView
+):  # LoginRequiredMixin should come first in the inheritance list to ensure that the user is authenticated before creating an article.
     model = Article
     template_name = "article_new.html"
     fields = ["title", "body"]
@@ -44,3 +44,44 @@ class ArticleCreateView(LoginRequiredMixin, CreateView): # LoginRequiredMixin sh
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class CommentGet(LoginRequiredMixin, DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object() #get_object only accessible through singleobjectmixin
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.object
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+
+class ArticleDetailView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
